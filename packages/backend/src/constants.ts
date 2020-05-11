0e8b4f83c4076d67ef81ba0705d7b8b23dc6fdf3
Jeremi Gendron <gendronjeremi@gmail.com>
2020-05-11 13:55:59 -0400

    add port based on NODE_ENV w/ constants

---
+++ packages/backend/src/constants.ts
@@ -0,0 +1,3 @@
+export const PROD_PORT = 80;
+export const DEV_PORT = 9000;
+export const TEST_PORT = 9001;
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -5,9 +5,9 @@ import { json } from "body-parser";
 import mongoose from "mongoose";
 import { routes } from "./routes";
 import { handleAssertionError } from "./errors";
+import { TEST_PORT, DEV_PORT, PROD_PORT } from "./constants";
 
 const app = express();
-const PORT = 9000;
 
 app.use(cors());
 app.use(json());
@@ -24,6 +24,14 @@ app.use(handleAssertionError);
 
 export async function setup(mongoUri: string): Promise<Server> {
   await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+  const PORT =
+    process.env.NODE_ENV === "production"
+      ? PROD_PORT
+      : process.env.NODE_ENV === "development"
+      ? DEV_PORT
+      : process.env.NODE_ENV === "test"
+      ? TEST_PORT
+      : 9000;
   return app.listen(PORT, () => {
     if (process.env.NODE_ENV !== "test") console.log(`Listening on port: ${PORT}`);
   });
